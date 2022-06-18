946483c696aa53bcdfb65f0dd86ae802d01062ab
Van Anh Le <anhlv.enactusftu@gmail.com>
2022-06-18 23:40:15 +0700

    Level 04: Handle multiple browser with BaseTest class

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,4 +1,37 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    private WebDriver driver;
+    private String projectPath = System.getProperty("user.dir");
+
+    protected WebDriver getBrowserDriver(String browserName) {
+        if (browserName.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+            driver = new FirefoxDriver();
+
+        } else if (browserName.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+            driver = new ChromeDriver();
+
+        } else if (browserName.equals("edge")) {
+            System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver");
+            driver = new EdgeDriver();
+
+        } else {
+            throw new RuntimeException("Invalid browser driver");
+        }
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+
+        return driver;
+    }
 }
