372444e4803cb5ffc93197c77d5f1b74fa734dd6
AnhLe1123 <82967714+AnhLe1123@users.noreply.github.com>
2022-06-20 10:42:24 +0700

    Level 04: Handle multiple browsers (#6)
    * Implement getBrowserDriver in BaseTest class. Test class extends BaseTest and uses TestNG parameters
    
    * Refactor getBrowserDriver method with WebDriverManager library

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,4 +1,71 @@
 package commons;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    private WebDriver driver;
+    private String projectPath = System.getProperty("user.dir");
+
+    protected WebDriver getBrowserDriver(String browserName) {
+        if (browserName.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        } else if (browserName.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        } else if (browserName.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+
+        } else if (browserName.equals("opera")) {
+            WebDriverManager.operadriver().setup();
+            driver = new OperaDriver();
+
+        } else if (browserName.equals("brave")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("/Applications/Brave Browser.app/Contents/MacOS/Brave Browser");
+            driver = new ChromeDriver(options);
+
+        } else if (browserName.equals("CocCoc")) {
+            WebDriverManager.chromedriver().driverVersion("101.0.4951.15").setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("/Applications/CocCoc.app/Contents/MacOS/CocCoc");
+            driver = new ChromeDriver(options);
+
+        } else if (browserName.equals("h_chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);
+            options.addArguments("window-size=1920x1080");
+            driver = new ChromeDriver(options);
+
+        } else if (browserName.equals("h_firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.setHeadless(true);
+            options.addArguments("window-size=1920x1080");
+            driver = new FirefoxDriver(options);
+
+        } else {
+            throw new RuntimeException("Invalid browser driver");
+        }
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+
+        return driver;
+    }
 }
