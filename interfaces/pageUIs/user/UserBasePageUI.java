ac179e903741e65ce68bd8cbff98eed90d0fad42
Van Anh Le <anhlv.enactusftu@gmail.com>
2022-06-22 21:36:42 +0700

    Level 09: Dynamic locator

--- interfaces/pageUIs/user/UserBasePageUI.java
+++ interfaces/pageUIs/user/UserBasePageUI.java
@@ -1,9 +1,10 @@
 package pageUIs.user;
 
 public class UserBasePageUI {
-    public static final String ADDRESSES_SIDEBAR_LINK = "//div[contains(@class, 'block-account-navigation')]//a[text()='Addresses']";
-    public static final String ORDERS_SIDEBAR_LINK = "//div[contains(@class, 'block-account-navigation')]//a[text()='Orders']";
-    public static final String REWARD_POINTS_SIDEBAR_LINK = "//div[contains(@class, 'block-account-navigation')]//a[text()='Reward points']";
-    public static final String CUSTOMER_INFO_SIDEBAR_LINK = "//div[contains(@class, 'block-account-navigation')]//a[text()='Customer info']";
-    public static final String LOGOUT_LINK = "//a[@class='ico-logout']";
+    public static final String ADDRESSES_SIDEBAR_LINK = "XPATH=//div[contains(@class, 'block-account-navigation')]//a[text()='Addresses']";
+    public static final String ORDERS_SIDEBAR_LINK = "Xpath=//div[contains(@class, 'block-account-navigation')]//a[text()='Orders']";
+    public static final String REWARD_POINTS_SIDEBAR_LINK = "XPath=//div[contains(@class, 'block-account-navigation')]//a[text()='Reward points']";
+    public static final String CUSTOMER_INFO_SIDEBAR_LINK = "xpath=//div[contains(@class, 'block-account-navigation')]//a[text()='Customer info']";
+    public static final String LOGOUT_LINK = "css=a[class='ico-logout']";
+    public static final String SIDEBAR_LINK_BY_PAGE_NAME = "xpath=//div[contains(@class, 'block-account-navigation')]//a[text()='%s']";
 }
