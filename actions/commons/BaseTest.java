58c74a67b7470054527eb2222c33fc826a4458d9
Van Anh Le <anhlv.enactusftu@gmail.com>
2022-06-28 14:20:43 +0700

    Level 12: Verify (Custom hard assert)

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.opera.OperaDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 import java.util.concurrent.TimeUnit;
 
@@ -141,5 +143,49 @@ public class BaseTest {
             default:
                 throw new RuntimeException("Invalid environment");
         }
+    }
+
+    protected boolean verifyTrue(boolean condition) {
+        boolean pass = true;
+        try {
+            Assert.assertTrue(condition);
+            System.out.println(" -------------------------- PASSED -------------------------- ");
+        } catch (Throwable e) {
+            System.out.println(" -------------------------- FAILED -------------------------- ");
+            pass = false;
+
+            // Add issues in ReportNG
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    protected boolean verifyFalse(boolean condition) {
+        boolean pass = true;
+        try {
+            Assert.assertFalse(condition);
+            System.out.println(" -------------------------- PASSED -------------------------- ");
+        } catch (Throwable e) {
+            System.out.println(" -------------------------- FAILED -------------------------- ");
+            pass = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    protected boolean verifyEquals(Object actual, Object expected) {
+        boolean pass = true;
+        try {
+            Assert.assertEquals(actual, expected);
+            System.out.println(" -------------------------- PASSED -------------------------- ");
+        } catch (Throwable e) {
+            pass = false;
+            System.out.println(" -------------------------- FAILED -------------------------- ");
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
     }
 }
